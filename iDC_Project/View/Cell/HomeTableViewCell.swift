d4cc21080afa4c505e992c2c88d42dc1506b312e
h-suo <phstoo1021@naver.com>
2023-02-28 13:46:19 +0900

    Commit Cell Constrains

--- iDC_Project/View/Cell/HomeTableViewCell.swift
+++ iDC_Project/View/Cell/HomeTableViewCell.swift
@@ -6,18 +6,77 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    
+    // - MARK: Setup UIKit
+    let titleLabel: UILabel = {
+        let tl = UILabel()
+        tl.font = UIFont.systemFont(ofSize: 18)
+        tl.textColor = .white
+        tl.numberOfLines = 1
         
+        return tl
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let dl = UILabel()
+        dl.font = UIFont.systemFont(ofSize: 14)
+        dl.textColor = .systemFill
+        dl.numberOfLines = 1
+        
+        return dl
+    }()
+    
+    let timeLabel: UILabel = {
+        let tl = UILabel()
+        tl.font = UIFont.systemFont(ofSize: 14)
+        tl.textColor = .systemFill
+        
+        return tl
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    // - MARK: Setup View
+    func setupView() {
+        
+        self.addSubview(titleLabel)
+        self.addSubview(descriptionLabel)
+        self.addSubview(timeLabel)
+        
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // - MARK: Setup Constraints
+    func setConstraints() {
+        
+        titleLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(descriptionLabel.snp.top).offset(-4)
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalToSuperview().offset(-12)
+        }
+        descriptionLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalToSuperview().offset(-12)
+        }
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(4)
+            make.leading.equalToSuperview().offset(12)
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         
     }
-
 }
--- iDC_Project/View/Cell/MessageTableViewCell.swift
+++ iDC_Project/View/Cell/MessageTableViewCell.swift
@@ -9,15 +9,73 @@ import UIKit
 
 class MessageTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    // - MARK: Setup UIKit
+    let nameLabel: UILabel = {
+        let nl = UILabel()
+        nl.font = UIFont.systemFont(ofSize: 18)
+        nl.textColor = .white
+        nl.numberOfLines = 1
         
+        return nl
+    }()
+    
+    let messageLabel: UILabel = {
+        let ml = UILabel()
+        ml.font = UIFont.systemFont(ofSize: 18)
+        ml.textColor = .systemFill
+        ml.numberOfLines = 1
+        
+        return ml
+    }()
+    
+    let timeLabel: UILabel = {
+        let tl = UILabel()
+        tl.font = UIFont.systemFont(ofSize: 14)
+        tl.textColor = .systemFill
+        
+        return tl
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    // - MARK: Setup View
+    func setupView() {
+        
+        self.addSubview(nameLabel)
+        self.addSubview(messageLabel)
+        self.addSubview(timeLabel)
+        
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // - MARK: Setup Constraints
+    func setConstraints() {
+        
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(12)
+            make.top.equalToSuperview().offset(4)
+            make.width.equalTo(40)
+        }
+        messageLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(4)
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalToSuperview().offset(12)
+        }
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(4)
+            make.trailing.equalToSuperview().offset(-12)
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         
     }
-
 }
